ca3066552453177d558b2d2c04927595af9b9654
1060237380 <1060237380@SJ109PC7.fup.edu>
2025-02-21 10:53:22 -0500

    Compus Validacio

--- src/main/java/Logica_Cliente/Main.java
+++ src/main/java/Logica_Cliente/Main.java
@@ -4,6 +4,7 @@
  */
 package Logica_Cliente;
 
+import Helpers.HelpersValidacion;
 import Logica_Negocio.Computador;
 import Logica_Negocio.Persona;
 import java.util.Scanner;
@@ -24,32 +25,161 @@ public class Main {
         String nombre, apellido, cedula, direccion;
         Computador objComputador;
         Persona objPersona;
+        int conteo, rta;
 
         System.out.println("Digite la marca del computador");
         marca = scan.nextLine();
+        rta = HelpersValidacion.ValidarVacio(marca);
+
+        while (rta > 0) {
+            System.out.println("Digite la marca del computador");
+            marca = scan.nextLine();
+            rta = HelpersValidacion.ValidarVacio(marca);
+        }
+
+        conteo = HelpersValidacion.ValidarTodo(marca);
+
+        while (conteo != 0) {
+            System.out.println("Digite la marca de la computador");
+            marca = scan.nextLine();
+            conteo = HelpersValidacion.ValidarTodo(marca);
+
+        }
 
         System.out.println("Digite el serial del computador");
         serial = scan.nextLine();
+        rta = HelpersValidacion.ValidarVacio(serial);
+
+        while (rta > 0) {
+            System.out.println("Digite el serial del computador");
+            serial = scan.nextLine();
+            rta = HelpersValidacion.ValidarVacio(serial);
+        }
+
+        conteo = HelpersValidacion.ValidarTodoSerial(serial);
+
+        while (conteo != 0) {
+            System.out.println("Digite el serial del computador");
+            serial = scan.nextLine();
+            conteo = HelpersValidacion.ValidarTodoSerial(serial);
+
+        }
 
         System.out.println("Digite el tamaño del computador");
         tamaño = scan.nextLine();
+        rta = HelpersValidacion.ValidarVacio(tamaño );
+
+        while (rta > 0) {
+            System.out.println("Digite el tamaño del computador");
+            tamaño  = scan.nextLine();
+            rta = HelpersValidacion.ValidarVacio(tamaño );
+        }
+
+        conteo = HelpersValidacion.ValidarTodo(tamaño );
+
+        while (conteo != 0) {
+            System.out.println("Digite el tamaño del computador");
+            tamaño  = scan.nextLine();
+            conteo = HelpersValidacion.ValidarTodo(tamaño );
+
+        }
 
         System.out.println("Digite el color del computador");
         color = scan.nextLine();
+        rta = HelpersValidacion.ValidarVacio(color);
+
+        while (rta > 0) {
+            System.out.println("Digite el color del computador");
+            color = scan.nextLine();
+            rta = HelpersValidacion.ValidarVacio(color);
+        }
+
+        conteo = HelpersValidacion.ValidarTodo(color);
+
+        while (conteo != 0) {
+            System.out.println("Digite el color del computador");
+            color = scan.nextLine();
+            conteo = HelpersValidacion.ValidarTodo(color);
+
+        }
         
         objComputador= new Computador(marca, serial, tamaño, color);
         
         System.out.println("Digite el nombre de la persona");
         nombre=scan.nextLine();
+        rta = HelpersValidacion.ValidarVacio(nombre);
+
+        while (rta > 0) {
+            System.out.println("Digite el nombre de la persona: ");
+            nombre = scan.nextLine();
+            rta = HelpersValidacion.ValidarVacio(nombre);
+        }
+
+        conteo = HelpersValidacion.ValidarTodo(nombre);
+
+        while (conteo != 0) {
+            System.out.println("Digite el nobre de la persona: ");
+            nombre = scan.nextLine();
+            conteo = HelpersValidacion.ValidarTodo(nombre);
+
+        }
         
-        System.out.println("Digite el apellido de la persona");
+        System.out.println("Digite el apellido de la persona: ");
         apellido=scan.nextLine();
+        rta = HelpersValidacion.ValidarVacio(apellido);
+
+        while (rta > 0) {
+            System.out.println("Digite el apellido de la persona:");
+            apellido = scan.nextLine();
+            rta = HelpersValidacion.ValidarVacio(apellido);
+        }
+
+        conteo = HelpersValidacion.ValidarTodo(apellido);
+
+        while (conteo != 0) {
+            System.out.println("Digite el apellido de la persona: ");
+            apellido = scan.nextLine();
+            conteo = HelpersValidacion.ValidarTodo(apellido);
+
+        }
         
-        System.out.println("Digite la cedula de la persona");
+        System.out.println("Digite la cedula de la persona: ");
         cedula=scan.nextLine();
+        rta = HelpersValidacion.ValidarVacio(cedula);
+
+        while (rta > 0) {
+            System.out.println("Digite la cedula de la persona: ");
+            cedula = scan.nextLine();
+            rta = HelpersValidacion.ValidarVacio(cedula);
+        }
+
+        conteo = HelpersValidacion.ValidarTodoLetra(cedula);
+
+        while (conteo != 0) {
+            System.out.println("Digite la cedula de la persona: ");
+            cedula = scan.nextLine();
+            conteo = HelpersValidacion.ValidarTodoLetra(cedula);
+
+        }
         
-        System.out.println("Digite la direccion de la persona");
+        System.out.println("Digite la direccion de la persona: ");
         direccion=scan.nextLine();
+        rta = HelpersValidacion.ValidarVacio(direccion);
+
+        while (rta > 0) {
+            System.out.println("Digite la direccion de la persona: ");
+            direccion = scan.nextLine();
+            rta = HelpersValidacion.ValidarVacio(direccion);
+        }
+
+        conteo = HelpersValidacion.ValidarTodoDireccion(direccion);
+
+        while (conteo != 0) {
+            System.out.println("Digite la direccion de la persona: ");
+            direccion = scan.nextLine();
+            conteo = HelpersValidacion.ValidarTodoDireccion(direccion);
+
+        }
         
         objPersona= new Persona(nombre, apellido, cedula, direccion, objComputador); //eestoy asociando
         
